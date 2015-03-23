955eba4c0e3c31f43e8efd742e9c91ba4a3ea3e0
HerrSchreiber <robert.schreiber@boiseschools.org>
2015-03-23 14:27:21 -0600

    Login and Registration validation started

--- handlers/login_handler.php
+++ handlers/login_handler.php
@@ -1,15 +1,44 @@
 <?php
 session_start();
-if ("admin@historicnerd.com" == $_POST["email"] &&
-    "lollipop" == $_POST["password"]) {
-    $_SESSION["user"] = "admin";
-    $_SESSION["privileges"] = 2;
-    header("Location:/");
+define("INVALID_USER_NAME_OR_PASSWORD", 1);
+define("PASSWORDS_DO_NOT_MATCH", 2);
+define("USERNAME_TOO_LONG", 4);
+define("NOT_A_VALID_EMAIL", 8);
+$_SESSION["status"] = 0;
+if ($_POST["submit"] == "Login") {
+    if ("admin@historicnerd.com" == $_POST["email"] &&
+        "lollipop" == $_POST["password"]
+    ) {
+        $_SESSION["user"] = "admin";
+        $_SESSION["privileges"] = 2;
+        header("Location:/");
 
-} else {
-    $status = "Invalid username or password";
-    $_SESSION["status"] = $status;
+    } else {
+        $_SESSION["status"] |= INVALID_USER_NAME_OR_PASSWORD;
+        $_SESSION["email_login_preset"] = $_POST["email"];
+
+        header("Location:/login.php");
+    }
+}
+else if ($_POST["submit"] == "Register") {
     $_SESSION["email_preset"] = $_POST["email"];
+    $_SESSION["user_preset"] = $_POST["user"];
+    if ($_POST["password"] != $_POST["password2"]) {
+        $_SESSION["status"] |= PASSWORDS_DO_NOT_MATCH;
+    }
+    if (strlen($_POST["user"]) > 35) {
+        $_SESSION["status"] |= USERNAME_TOO_LONG;
+    }
 
-    header("Location:/login.php");
+    if (preg_match("/^.+@.+\..{2,4}$/", $_POST["email"]) == 0) {
+        $_SESSION["status"] |= NOT_A_VALID_EMAIL;
+    }
+
+    if ($_SESSION["status"] == 0) {
+        $_SESSION["user"] = $_POST["user"];
+        header("Location:/");
+    }
+    else {
+        header("Location:/login.php");
+    }
 }
--- login.php
+++ login.php
@@ -1,34 +1,75 @@
 <?php
 require "/views/header.php";
+define("INVALID_USER_NAME_OR_PASSWORD", 1);
+define("PASSWORDS_DO_NOT_MATCH", 2);
+define("USERNAME_TOO_LONG", 4);
+define("NOT_A_VALID_EMAIL", 8);
 
+$loginEmail = isset($_SESSION["email_login_preset"])?$_SESSION["email_login_preset"]:"";
 $email = isset($_SESSION["email_preset"])?$_SESSION["email_preset"]:"";
+$user = isset($_SESSION["user_preset"])?$_SESSION["user_preset"]:"";
 ?>
 
     <div id="container">
         <div id="log-in"><h2>Log In</h2>
             <form action="/handlers/login_handler.php" method="POST">
                 <div>
-                    <label for="email">Email</label>
-                    <input type="text" name="email" id="email" value="<?php echo $email; ?>"/>
+                    <label for="login-email">Email</label>
+                    <input type="text" name="email" id="login-email" value="<?php echo $loginEmail; ?>"/>
                 </div>
                 <div>
-                    <label for="password">Password</label>
-                    <input type="password" name="password" id="password" value=""/>
+                    <label for="login-password">Password</label>
+                    <input type="password" name="password" id="login-password" value=""/>
                 </div>
-                <?php if (isset($_SESSION["status"])) { ?>
-                <div class="error">
-                    <p><?php echo $_SESSION["status"]?></p>
+                <?php if (isset($_SESSION["status"]) && $_SESSION["status"] & INVALID_USER_NAME_OR_PASSWORD === INVALID_USER_NAME_OR_PASSWORD) { ?>
+                <div class="error-message">
+                    <p>Invalid username or password</p>
                 </div>
 
                 <?php }?>
                 <div>
-                    <input type="submit" name="submit" id="login" value="Login"/>
+                    <input type="submit" name="submit" value="Login"/>
                 </div>
             </form>
         </div>
 
         <div id="register"><h2>Register</h2>
-
+            <form action="/handlers/login_handler.php" method="POST">
+                <div>
+                    <label for="user">User Name</label>
+                    <input type="text" name="user" id="user" value="<?php echo $user; ?>"/>
+                </div>
+                <?php if (isset($_SESSION["status"]) && ($_SESSION["status"] & USERNAME_TOO_LONG) === USERNAME_TOO_LONG) { ?>
+                    <div class="error-message">
+                        <p>User name is too long</p>
+                    </div>
+                <?php }?>
+                <div>
+                    <label for="email">Email</label>
+                    <input type="text" name="email" id="email" value="<?php echo $email; ?>"/>
+                </div>
+                <?php if (isset($_SESSION["status"]) && ($_SESSION["status"] & NOT_A_VALID_EMAIL) === NOT_A_VALID_EMAIL) { ?>
+                    <div class="error-message">
+                        <p>Email is not valid</p>
+                    </div>
+                <?php }?>
+                <div>
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" value=""/>
+                </div>
+                <div>
+                    <label for="password2">Confirm Password</label>
+                    <input type="password" name="password2" id="password2" value=""/>
+                </div>
+                <?php if (isset($_SESSION["status"]) && ($_SESSION["status"] & PASSWORDS_DO_NOT_MATCH) === PASSWORDS_DO_NOT_MATCH) { ?>
+                    <div class="error-message">
+                        <p>Passwords do not match</p>
+                    </div>
+                <?php }?>
+                <div>
+                    <input type="submit" name="submit" value="Register"/>
+                </div>
+            </form>
         </div>
     </div>
 
