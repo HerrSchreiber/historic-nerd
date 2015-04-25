87c9b90064a12e1c3e4faafdff21a09c1a1cffae
herrschreiber <robert.schreiber@boiseschools.org>
2015-04-24 20:37:55 -0600

    finished ajax

--- js/ajax.js
+++ js/ajax.js
@@ -1,28 +1,29 @@
-$("#comments-form").submit(function(e){
-	alert("script running");
-	var values = $("#comments-form").serialize();
-	var comment = $("#comment").val();
-	if (comment.trim() == "" || comment == "Enter comment here...") {
-		$("#comment").after("<div class=\"error-message\"><p>Your comment must be between 0 and 500 characters</p></div>");
-		return false;
-	}
-	else {
-		alert(comment);
-		$.ajax({
-			type: "POST",
-			url: "/~rschreib/handlers/comment_handler.php",
-			data: values,
-			success: function() {
-				$("#comments").append("<div><h4>You posted just now:</h4><p>"+escapeHTML(comment)+"</p></div>");
-				$("#comment").val("");
-			},
-			error: function () {
-				alert("FAILURE");
-			}
-		});
-		e.preventDefault();
-		return false;
-	}
+$(document).ready(function(){
+	$("#comments-form").submit(function(e){
+		$(".error-message").remove();
+		var values = $("#comments-form").serialize();
+		var comment = $("#comment").val();
+		if (comment.trim() == "" || comment == "Enter comment here..." || comment.length > 500) {
+			$("#comment").after("<div class=\"error-message\"><p>Your comment must be between 0 and 500 characters</p></div>");
+			$(".error-message").fadeOut(5000);
+			return false;
+		}
+		else {
+			$.ajax({
+				type: "POST",
+				url: "/~rschreib/handlers/comment_handler.php",
+				data: values,
+				success: function() {
+					$("#comments").append("<div><h4>You posted just now:</h4><p>"+escapeHTML(comment)+"</p></div>");
+					$("#comment").val("");
+				},
+				error: function () {
+					alert("FAILURE");
+				}
+			});
+			return false;
+		}
+	});
 });
 
 function escapeHTML(text) {
