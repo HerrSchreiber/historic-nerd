0ebda6e46c12f2857a1da307b1d7c81eadbb651a
herrschreiber <robert.schreiber@boiseschools.org>
2015-04-24 19:46:00 -0600

    ajax comment validation added

--- js/ajax.js
+++ js/ajax.js
@@ -1,21 +1,38 @@
 $("#comments-form").submit(function(e){
-	alert("The function got called");
+	alert("script running");
 	var values = $("#comments-form").serialize();
 	var comment = $("#comment").val();
-	console.log(values);
-	$.ajax({
-		type: "POST",
-		url: "/~rschreib/handlers/comment_handler.php",
-		data: values,
-		success: function() {
-			$("#comments").append("<div><h4>You posted just now:</h4><p>"+comment+"</p></div>");
-			$("#comment").val("");
-		},
-		error: function () {
-			alert("FAILURE");
-		}
-	});
-	e.preventDefault();
-	return false;
+	if (comment.trim() == "" || comment == "Enter comment here...") {
+		$("#comment").after("<div class=\"error-message\"><p>Your comment must be between 0 and 500 characters</p></div>");
+		return false;
+	}
+	else {
+		alert(comment);
+		$.ajax({
+			type: "POST",
+			url: "/~rschreib/handlers/comment_handler.php",
+			data: values,
+			success: function() {
+				$("#comments").append("<div><h4>You posted just now:</h4><p>"+escapeHTML(comment)+"</p></div>");
+				$("#comment").val("");
+			},
+			error: function () {
+				alert("FAILURE");
+			}
+		});
+		e.preventDefault();
+		return false;
+	}
 });
-$(document).ready(function(){alert("hi");});
+
+function escapeHTML(text) {
+	var map = {
+		'&': '&amp;',
+		'<': '&lt;',
+		'>': '&gt;',
+		'"': '&quot;',
+		"'": '&#039;'
+	};
+
+	return text.replace(/[&<>"]/g, function(m) { return map[m]; });
+}
